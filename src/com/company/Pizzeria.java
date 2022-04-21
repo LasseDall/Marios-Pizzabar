528fb936a2c5db8a5450292cdc2190d6fa862bf1
LasseDall <lass794rrstud.kea.dk>
2022-04-21 13:29:25 +0200

    kørProgram tilføjet

--- src/com/company/Pizzeria.java
+++ src/com/company/Pizzeria.java
@@ -5,31 +5,56 @@ import java.util.Scanner;
 public class Pizzeria {
     private MenuKort menuKort;
     private BestillingsListe bestillingsListe;
+    UI ui = new UI();
+    boolean programKører = true;
+    Scanner sc = new Scanner(System.in);
 
-    public Pizzeria(){
-        bestillingsListe=new BestillingsListe();
+
+    public Pizzeria() {
+        bestillingsListe = new BestillingsListe();
 
     }
 
-    public void controller() {
-        lavBestilling(1,0015);
-        //menuKort.visMenu();
-    }
-
-    public void lavBestilling(int antalPizzaer,int afhentning){
-        Scanner keyboard = new Scanner(System.in);
-        Bestilling bestilling = new Bestilling(afhentning);
-        while (antalPizzaer>0){
-            System.out.println("pizza nummer");
-            int pizzaNummer = keyboard.nextInt();
-            Pizza tmp = menuKort.findPizza(pizzaNummer);
-            bestilling.tilføj(tmp);
-            antalPizzaer--;
+    public void kørProgram() {
+        ui.velkommen();
+        while (programKører) {
+            ui.mainMenu();
+            int valg = sc.nextInt();
+            valgMainMenu(valg);
         }
-        System.out.println(bestilling);
-        bestillingsListe.tilføj(bestilling);
     }
-    public void oversigt(){
-        System.out.println(bestillingsListe.toString());
+
+        public void controller() {
+            lavBestilling(1, 0015);
+            //menuKort.visMenu();
+        }
+
+        public void lavBestilling(int antalPizzaer, int afhentning) {
+            Scanner keyboard = new Scanner(System.in);
+            Bestilling bestilling = new Bestilling(afhentning);
+            while (antalPizzaer > 0) {
+                System.out.println("pizza nummer");
+                int pizzaNummer = keyboard.nextInt();
+                Pizza tmp = menuKort.findPizza(pizzaNummer);
+                bestilling.tilføj(tmp);
+                antalPizzaer--;
+            }
+            System.out.println(bestilling);
+            bestillingsListe.tilføj(bestilling);
+        }
+
+        public void oversigt() {
+            System.out.println(bestillingsListe.toString());
+        }
+
+    public void valgMainMenu(int valg) {
+        switch (valg) {
+            case 1 -> ui.visMenukort();
+            case 2 -> controller();
+            case 3 -> oversigt();
+            //case 4 -> fjernBestilling();
+            case 5 -> programKører = false;
+        }
     }
-}
+    }
+
